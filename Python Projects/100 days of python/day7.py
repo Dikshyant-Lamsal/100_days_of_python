1aa13eeef20c235ad0bc09fe34ee578761fc9543
Dikshyant-Lamsal <140510854+Dikshyant-Lamsal@users.noreply.github.com>
2025-03-01 23:40:22 +0545

    hangman 1/3/2025 23:40

--- Python Projects/100 days of python/day7.py
+++ Python Projects/100 days of python/day7.py
@@ -3,13 +3,21 @@ word_list=["Hello","Word","Onomotopoeia","Hangman","Einstein","Oppenheimer","Gra
 chosen_word=(random.choice(word_list)).lower()
 
 print(chosen_word)
+placeholder=""
+
+    
+for i in range(len(chosen_word)):
+    placeholder+="_"
 
 
 guess=input("Guess a Letter: ").lower()
-
+display=""
 for letter in chosen_word:
-    if guess==letter:
-        print("Right")
-    else:
-        print("Wrong")
-        life = life - 1
+        if guess==letter:
+            display+=guess
+            flag=1
+        else:
+            display+="_"
+print(display)
+    
+        
