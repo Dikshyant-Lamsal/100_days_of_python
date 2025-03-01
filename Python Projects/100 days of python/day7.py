4605ee48f11b29e40e0202b9444ad803db6cfb72
Dikshyant-Lamsal <dikshyant01@gmail.com>
2025-03-01 20:45:50 +0530

    comeeat

--- Python Projects/100 days of python/day7.py
+++ Python Projects/100 days of python/day7.py
@@ -1,1 +1,15 @@
-print("Day 7!")
+import random
+word_list=["Hello","Word","Onomotopoeia","Hangman","Einstein","Oppenheimer","Gracilis","Momentum","Ballerina"]
+chosen_word=(random.choice(word_list)).lower()
+
+print(chosen_word)
+
+
+guess=input("Guess a Letter: ").lower()
+
+for letter in chosen_word:
+    if guess==letter:
+        print("Right")
+    else:
+        print("Wrong")
+        life = life - 1
