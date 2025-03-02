b41815e37014744be377ae91dabc82413da1cf9e
Dikshyant-Lamsal <dikshyant01@gmail.com>
2025-03-02 20:54:33 +0530

    hangaman 20:54

--- Python Projects/100 days of python/day7.py
+++ Python Projects/100 days of python/day7.py
@@ -9,14 +9,25 @@ placeholder=""
 for i in range(len(chosen_word)):
     placeholder+="_"
 
-guess = input("Guess a letter: ").lower()
 
-display=""
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
+display_list=list(placeholder)
+correct=""
+length=len(placeholder)
 
-print(display)
+
+while(length>0):
+    i=-1
+    j=-1
+    guess = input("Guess a letter: ").lower()
+    for letter in chosen_word:
+        i+=1
+        if letter == guess:
+            display_list[i] = letter
+            length-=1
+        elif display_list[i]!=letter:
+            display_list[i] = "_"
+
+    print("".join(display_list))
+
+
