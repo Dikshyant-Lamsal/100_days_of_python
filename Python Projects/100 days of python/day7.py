80f0f7e29b5900daf8aeea8ff1f26e3fe23f34e5
Dikshyant-Lamsal <dikshyant01@gmail.com>
2025-03-02 21:58:55 +0530

    hangman almost complete

--- Python Projects/100 days of python/day7.py
+++ Python Projects/100 days of python/day7.py
@@ -1,47 +1,97 @@
 import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list=["Hello","Word","Onomotopoeia","Hangman","Einstein","Oppenheimer","Gracilis","Momentum","Ballerina"]
 chosen_word=(random.choice(word_list)).lower()
 
 print(chosen_word)
-placeholder=""
-
-    
-for i in range(len(chosen_word)):
-    placeholder+="_"
-
-
-
-display_list=list(placeholder)
 
 game_over=False
 false_guess=0
+correct_guess=[]
+lives=6
 
 while(not game_over):
-    i=-1
-    j=-1
-    guess = input("Guess a letter: ").lower()
     
+    guess = input("Guess a letter: ").lower()
+    display=""    
 
     for letter in chosen_word:
-        # display_list_prev=display_list
-        i+=1
         if letter == guess:
-            display_list[i] = letter
-        elif display_list[i]!=letter:
-            display_list[i] = "_"
-        
-    # if(display_list_prev==display_list):
-    #     false_guess+=1        
-    #     print(f"False Guess: {false_guess}")
+            display += letter
+            correct_guess.append(letter)
+        elif letter in correct_guess:
+            display += letter
+        else:
+            display += "_"
 
-    print("".join(display_list))
+    if(guess in chosen_word):
+        print(stages[lives]) 
+        print(display)   
+    else:
+        lives-=1
+        print(f"Incorrect Guess, Lives Remaining: {lives}")
+        print(display)
+        print(stages[lives])
 
-    if("_" not in display_list):
+    if("_" not in list(display)):
         print("YOU WIN!")
         game_over=True
-    elif(false_guess==5):
+    elif(lives==0):
+        print("You Lose")
+        print(stages[0])
         game_over=True
-
-    
-
-
