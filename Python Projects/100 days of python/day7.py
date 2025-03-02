87eb53a6cffca984bda3fb4866f60c5f5c7c16c5
Dikshyant-Lamsal <dikshyant01@gmail.com>
2025-03-02 21:16:12 +0530

    comiiiiit

--- Python Projects/100 days of python/day7.py
+++ Python Projects/100 days of python/day7.py
@@ -12,22 +12,36 @@ for i in range(len(chosen_word)):
 
 
 display_list=list(placeholder)
-correct=""
-length=len(placeholder)
 
+game_over=False
+false_guess=0
 
-while(length>0):
+while(not game_over):
     i=-1
     j=-1
     guess = input("Guess a letter: ").lower()
+    
+
     for letter in chosen_word:
+        # display_list_prev=display_list
         i+=1
         if letter == guess:
             display_list[i] = letter
-            length-=1
         elif display_list[i]!=letter:
             display_list[i] = "_"
+        
+    # if(display_list_prev==display_list):
+    #     false_guess+=1        
+    #     print(f"False Guess: {false_guess}")
 
     print("".join(display_list))
+
+    if("_" not in display_list):
+        print("YOU WIN!")
+        game_over=True
+    elif(false_guess==5):
+        game_over=True
+
+    
 
 
