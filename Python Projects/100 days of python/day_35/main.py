38d6ef8939907c2785fe1aeb8afed7fc12e1839d
Dikshyant-Lamsal <140510854+Dikshyant-Lamsal@users.noreply.github.com>
2026-03-06 23:25:52 +0530

    added .env openwaether api with rain check function

---
+++ Python Projects/100 days of python/day_35/main.py
@@ -0,0 +1,39 @@
+import requests
+import datetime
+import geocoder
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+lat = geocoder.ip('me').latlng[0]
+lon = geocoder.ip('me').latlng[1]
+
+api_key = {os.getenv("api_key")}
+print(api_key)
+open_weather_params = {
+    "lat": lat,
+    "lon": lon,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=open_weather_params)
+response.raise_for_status()
+data = response.json()
+data_list = data["list"]
+
+
+def will_rain():
+    for i in range(0, 4):
+        print(data_list[i]["weather"])
+        weather_id = data_list[i]["weather"][0]["description"]
+        print(weather_id)
+        if weather_id == "rain":
+            return True
+    return False
+
+if will_rain():
+    print("Bring an umbrella.")
+else:
+    print("No rain today.")
